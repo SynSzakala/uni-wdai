f97b83c0c91ee69dc3757b302c9c490568728186
kosciolek <45483493+kosciolek@users.noreply.github.com>
2022-11-20 12:56:20 +0100

    test env

--- terraform/test-env/outputs.tf
+++ terraform/test-env/outputs.tf
@@ -1,16 +1,3 @@
-output "mongo_url" {
-  value = aws_docdb_cluster.mongo.endpoint
-}
-
-output "mongo_username" {
-  value = aws_docdb_cluster.mongo.master_username
-}
-
-output "mongo_password" {
-  sensitive = true
-  value     = aws_docdb_cluster.mongo.master_password
-}
-
 output "input_bucket_name" {
   value = aws_s3_bucket.input.bucket
 }
