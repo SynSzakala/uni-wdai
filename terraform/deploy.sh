cb918afafebff9499844da8b8e00532c842730de
kosciolek <45483493+kosciolek@users.noreply.github.com>
2023-01-08 00:19:57 +0100

    ecr auth

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -13,7 +13,7 @@ docker build -f ../Dockerfile-worker -t $CONVERTER_REPOSITORY_URL ..
 docker push $CONVERTER_REPOSITORY_URL
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $AUTH_REPOSITORY_URL
-docker build -f ../Dockerfile-worker -t $AUTH_REPOSITORY_URL ..
+docker build -f ../Dockerfile-api -t $AUTH_REPOSITORY_URL ..
 docker push $AUTH_REPOSITORY_URL
 
 terraform -chdir=main plan -var="api_repository_url=$API_REPOSITORY_URL" -var="converter_repository_url=$CONVERTER_REPOSITORY_URL" -var="auth_repository_url=$AUTH_REPOSITORY_URL" -out=tfplan
